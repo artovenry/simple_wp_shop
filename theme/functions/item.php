4149c7012e54c7a421db588279f711651468c7f0
vegeta <dev@vegeta.co.jp>
2019-04-22 13:34:29 +0900

    CMT

--- theme/functions/item.php
+++ theme/functions/item.php
@@ -12,19 +12,65 @@ add_action("init", function(){
       ["title", "editor", "thumbnail", "excerpt", "revisions"],
   ];
 
-  // META_BOXES
-    $renderer= function($name){
-      e= function($post, $args){Phug::displayFile(__DIR__ . "/item/selling_price.pug", ["post"=>$post, "args"=>$args],["expressionLanguage"=>"php"]);};
 
-    add_meta_box(, $selling_price, get_current_screen(), "side", "core");
+  // META_KEYS AND VALUES
+    $meta_key= function($key)use($NAME){
+      return "${NAME}_${key}";
+    };
+    $meta_value= function($post, $slug)use($NAME, $meta_key){
+      return get_post_meta($post->ID, $meta_key($slug), true);
+    };
+
+  // META_BOXES
     $boxes=[
-      ["name"=> "selling_price", "label"=>"販売価格"],
-    ]
-    $options["register_meta_box_cb"]= function()use($NAME, $boxes){
+      [
+        "slug"=> "selling_price", "label"=>"販売価格", "callback"=>function($post_id, $post, $updated)use($meta_key){
+          $key= $meta_key("selling_price");
+          $value= $_POST[$key];
+          if(isset($value))update_post_meta($post_id, $key, $value);
+        }
+      ],
+    ];
+
+    $nonce= function($slug)use($NAME){
+      $key= "_metabox_${NAME}_${slug}";
+      $name="_nonce${key}";
+      return ["key" =>$key, "name" => $name];
+    };
+
+    $renderer= function($slug)use(
+      $nonce, $meta_key, $meta_value
+    ){
+      return function($post, $args) use(
+        $slug, $nonce, $meta_key, $meta_value
+      ){
+        wp_nonce_field($nonce($slug)["key"], $nonce($slug)["name"]);
+        Phug::displayFile(__DIR__ . "/item/${slug}.pug", [
+          "post"=>$post,
+          "args"=>$args,
+          "meta_key"=>$meta_key,
+          "meta_value"=>$meta_value
+        ],["expressionLanguage"=>"php"]);
+      };
+    };
+
+    $options["register_meta_box_cb"]= function()use($NAME, $boxes, $renderer){
       foreach($boxes as $item){
-        add_meta_box(join("_", [$NAME, $item["name"]]),$item["label"], $renderer($item["name"]), get_current_screen(), "side", "core");
+        add_meta_box(join("_", [$NAME, $item["slug"]]),$item["label"], $renderer($item["slug"]), get_current_screen(), "side", "core");
       }
     };
+
+    add_action("save_post_${NAME}", function($post_id, $post, $updated)use($boxes, $nonce){
+      if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+      if(!current_user_can("edit_post", $post->ID)) return;
+      if(!is_user_logged_in()) return;
+      foreach($boxes as $item){
+        $key= $nonce($item['slug'])['key'];
+        $name= $nonce($item['slug'])['name'];
+        if(!wp_verify_nonce($_POST[$name], $key))return;
+        if(is_callable($item["callback"]))$item["callback"]($post_id, $post, $updated);
+      }
+    }, 10, 3);
 
   $routes= [
     // ARCHIVE
